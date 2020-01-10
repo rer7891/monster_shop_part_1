db9eb4d682c8821467c6f1235420f297d3776f1d
Becky Robran <rerobran@gmail.com>
2020-01-10 12:25:47 -0700

    updates validations

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,8 +2,8 @@ class Coupon < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :code, presence: true, uniqueness: true
-  validates_inclusion_of :used?, in: [true, false]
-  validates_inclusion_of :percent, in: 0..100
+  validates_presence_of :used?, in: [true, false]
+  validates_presence_of :percent, in: 0..100
 
   belongs_to :merchant
 
