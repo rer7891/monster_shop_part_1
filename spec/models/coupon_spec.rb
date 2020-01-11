57d1470275888571bb42149e48b28a1d47e9c266
Becky Robran <rerobran@gmail.com>
2020-01-10 20:33:15 -0700

    removes used validation from coupon model test

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -5,7 +5,6 @@ describe Coupon, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :code }
     it { should validate_presence_of :percent }
-    it { should validate_presence_of :used? }
   end
 
   describe "relationships" do
