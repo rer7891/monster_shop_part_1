c6d9298519b73b9d2c80b1ffff86581bd98b1517
Becky Robran <rerobran@gmail.com>
2020-01-13 15:41:07 -0700

    adds new action to coupon controller

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -10,4 +10,33 @@ class Merchant::CouponsController <  Merchant::BaseController
   def show
     @coupon = Coupon.find(params[:id])
   end
+
+  def new
+    merchant = current_user.merchant
+    coupon_count = merchant.coupons.where(active: true).count
+    if coupon_count <= 4
+      @coupon = Coupon.new
+    else
+      flash[:error] = "Deactivate a coupon if you wish to add more."
+      redirect_to merchant_dash_coupons_path
+    end
+  end
+
+  def create
+    merchant = current_user.merchant
+    coupon = merchant.coupons.new(coupon_params)
+    if coupon.save
+      flash[:success] = "You added a new coupon."
+      redirect_to merchant_dash_coupons_path
+    else
+      generate_error(coupon)
+      render :new
+    end
+  end
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :code, :percent)
+  end
 end
