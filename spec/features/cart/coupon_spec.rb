a79ed8846e7d0b2e4a6aeea6fbd9cb573a163468
Becky Robran <rerobran@gmail.com>
2020-01-13 08:27:16 -0700

    adds to cart coupon spec

--- spec/features/cart/coupon_spec.rb
+++ spec/features/cart/coupon_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe "As a user", type: :feature do
   @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
   @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
-  @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+  @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 135, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
   @seat = @meg.items.create(name: "Leather Seat", description: "They'll never wear out", price: 300, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 16)
   @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
   @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
-  @coupon_1 = create(:random_coupon, merchant: @meg)
+  @coupon_1 = create(:random_coupon, percent: 0.43, merchant: @meg)
 
   visit "/items/#{@paper.id}"
   click_on "Add To Cart"
@@ -30,6 +30,11 @@ RSpec.describe "As a user", type: :feature do
   it "when I enter a coupon it only discounts the items from that merchant" do
     fill_in "Code", with: @coupon_1.code
     click_button "Create Coupon"
+
+    within "cart-item-#{@tire.id}" do
+      expect(page).to have_content(@tire.price)
+      expect(page).to have_content(@tire.price/@coupon_1.percent)
+    end
   end
 
   xit "I can continue shopping after entering code and coupon will be there when I return" do
