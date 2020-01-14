cdc91020f07526c6a66c880f024582d6d55beaf3
Becky Robran <rerobran@gmail.com>
2020-01-14 16:17:43 -0700

    Testing to add a coupon to an order

--- spec/features/user/orders/coupon_spec.rb
+++ spec/features/user/orders/coupon_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe "As a user", type: :feature do
     @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-    @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
-    @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+    @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 250, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+    @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 210, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
     @coupon_1 = create(:random_coupon, percent: 0.43, merchant: @mike)
 
     visit "/items/#{@paper.id}"
@@ -29,10 +29,22 @@ RSpec.describe "As a user", type: :feature do
     fill_in :city, with: @user.city
     fill_in :state, with: @user.state
     fill_in :zip, with: @user.zip
+  end
+
+  it "can attach a discount to an order" do
+    #once a coupon is used it's status is updated
+    expect(@coupon_1.used?).to eq(false)
 
     click_button "Create Order"
-  end
-  it "can attach a discount to an order" do
+
+    @coupon_1.reload
+    expect(@coupon_1.used?).to eq(true)
+
     order = Order.last
+    expect(order.coupon).to eq(@coupon_1)
+
+    #can show discounted order totals and savings"
+      expect(order.discounted_grand_total).to eq(3.63)
+      expect(order.savings).to eq(1.97)
   end
 end
