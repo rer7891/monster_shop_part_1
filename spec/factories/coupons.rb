7860851654bb79784290e6026f0039088cbd6608
Becky Robran <rerobran@gmail.com>
2020-01-14 11:18:57 -0700

    adds seed coupon for rails s

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -2,9 +2,18 @@ FactoryBot.define do
   factory :random_coupon, class: Coupon do
     name              {Faker::FunnyName.name}
     code              {Faker::Code.asin}
-    percent           {rand(1..100)}
+    percent           {0.35}
     used?             {false}
     active            {true}
     association       :merchant, factory: :jomah_merchant
   end
+
+  factory :coupon_1, class: Coupon do
+   name      {"Holiday Sale"}
+   code      {"HOLIDAY1"}
+   percent?  {0.40}
+   used?     {true}
+   active    {true}
+   association       :merchant, factory: :jomah_merchant
+ end
 end
