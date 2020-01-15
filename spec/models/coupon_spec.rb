30b5361171879a7ecadc3e8542b46a7cb8f2e0bc
Becky Robran <rerobran@gmail.com>
2020-01-15 13:08:31 -0700

    Adds model testing and adds to coupon feature spec

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -12,4 +12,20 @@ describe Coupon, type: :model do
     it {should have_many :user_coupons}
     it {should have_many(:users).through(:user_coupons)}
   end
+
+  describe "methods" do
+    # def uses(coupon, user)
+    #   UserCoupon.where(user_id: user.id).where(coupon_id: coupon.id).count
+    # end
+      it "should be able to count coupon uses by user" do
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      coupon_1 = create(:random_coupon, percent: 0.43, merchant: meg)
+
+      user = create(:regular_user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      user.user_coupons.create(coupon: coupon_1)
+
+      expect(coupon_1.uses(user)).to eq(1)
+    end
+  end
 end
