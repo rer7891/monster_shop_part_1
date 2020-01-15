63c489ea5ef101c4dfd1924577c41a543e62cfaa
Becky Robran <rerobran@gmail.com>
2020-01-15 13:06:43 -0700

    Adds coupon can only be used once

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,7 +1,12 @@
 class CouponsController < ApplicationController
   def create
-    session[:coupon] = params["coupon"]["code"]
-    @coupon = session[:coupon]
+    coupon = Coupon.where(code: params["coupon"]["code"]).first
+      if current_user.id != nil && coupon.uses(current_user) < 1
+      session[:coupon] = params["coupon"]["code"]
+      coupon = session[:coupon]
+    else
+      flash[:error] = "Please log in to use this coupon or check that the coupon has not already been used."
+    end
     redirect_to cart_path
   end
 end
